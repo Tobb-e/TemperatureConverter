cb91067458a463055ef4345277be7365e06a5e1b
Tobbe <tobben.nilsson@gmail.com>
2024-08-14 07:51:27 +0200

    Separated ITemperatureConverter class

---
+++ ITemperatureConverter.cs
@@ -0,0 +1,8 @@
+namespace TemperatureConverter
+{
+    // Define an interface for temperature conversion
+    public interface ITemperatureConverter
+    {
+        double Convert(double temperature);
+    }
+}
